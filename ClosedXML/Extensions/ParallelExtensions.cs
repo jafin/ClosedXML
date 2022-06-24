57ba2dee39f3ebc1d32bbdcf77f3f195243076b2
Jason Finch <jason.finch@gmail.com>
2022-06-24 21:53:10 +1000

    refactor (Perf): minor performance gains
    XLFontKey & XLStyleKey to readonly structs.
    Parallelize PrepareBitMatrix & cfRules.

---
+++ ClosedXML/Extensions/ParallelExtensions.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClosedXML.Extensions
+{
+    public static class ParallelExtensions
+    {
+        public static IEnumerable<T1> OrderedParallel<T, T1>(this IEnumerable<T> list, Func<T, T1> action)
+        {
+            var unorderedResult = new ConcurrentBag<(long, T1)>();
+            Parallel.ForEach(list, (o, state, i) => { unorderedResult.Add((i, action.Invoke(o))); });
+            var ordered = unorderedResult.OrderBy(o => o.Item1);
+            return ordered.Select(o => o.Item2);
+        }
+    }
+}
